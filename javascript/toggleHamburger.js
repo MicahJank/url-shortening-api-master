b0fa462a0081b7910b2d10b9b587e78c7ee1093f
Micah <micahjank@gmail.com>
2020-09-27 17:05:31 -0500

    got main layout at 375px looking better

--- javascript/toggleHamburger.js
+++ javascript/toggleHamburger.js
@@ -1,10 +1,14 @@
+
 // grabbing the hamburger button
 const menuBtn = document.querySelector('.hamburger');
-
 menuBtn.addEventListener('click', () => {
+    const nav = document.querySelector('nav');
     if(menuBtn.classList.contains('is-active')) {
-        menuBtn.classList.remove('is-active')
+        menuBtn.classList.remove('is-active');
     } else {
         menuBtn.classList.add('is-active');
     }
+
+    nav.classList.toggle('visible');
+    
 })
